ea2612bb87e522b7daee823e568e7ad6a38d26c5
Gabriel Amaral <ga-amaral@live.com>
2020-08-01 23:45:32 -0300

    Programa feito como a Udemy solicitou

---
+++ HashSolo/Entities/Users.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashSolo.Entities
+{
+    class Users
+    {
+        public int IdCode { get; set; }
+
+        public Users(int idCode)
+        {
+            IdCode = idCode;
+        }
+
+        public override int GetHashCode()
+        {
+            return IdCode.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Users))
+            {
+                return false;
+            }
+            Users other = obj as Users;
+            return IdCode.Equals(other.IdCode);
+        }
+    }
+}
---
+++ HashSolo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using HashSolo.Entities;
+
+namespace HashSolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Users> users = new HashSet<Users>();
+
+            Console.Write("How many students for course A? ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n1; i++)
+            {
+                int ID = int.Parse(Console.ReadLine());
+                users.Add(new Users(ID));
+            }
+
+            Console.Write("How many students for course B? ");
+            n1 = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n1; i++)
+            {
+                int ID = int.Parse(Console.ReadLine());
+                users.Add(new Users(ID));
+            }
+            
+            Console.Write("How many students for course A? ");
+            n1 = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n1; i++)
+            {
+                int ID = int.Parse(Console.ReadLine());
+                users.Add(new Users(ID));
+            }
+            Console.WriteLine("Total students: " + users.Count);
+        }
+    }
+}
