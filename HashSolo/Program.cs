fc6900acff18349bf38a03cee871a1c224abba72
Gabriel Amaral <ga-amaral@live.com>
2020-08-01 23:47:28 -0300

    Atualização na lógica com Char[] e for

--- HashSolo/Program.cs
+++ HashSolo/Program.cs
@@ -9,32 +9,20 @@ namespace HashSolo
         static void Main(string[] args)
         {
             HashSet<Users> users = new HashSet<Users>();
+            char[] letra = { 'A', 'B', 'C' };
 
-            Console.Write("How many students for course A? ");
-            int n1 = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < n1; i++)
+            //Roda pelas letras
+            for (int i = 0; i < 3; i++)
             {
-                int ID = int.Parse(Console.ReadLine());
-                users.Add(new Users(ID));
-            }
+                //Pergunta as classes
+                Console.Write($"How many students for course {letra[i]}? ");
+                int n1 = int.Parse(Console.ReadLine());
 
-            Console.Write("How many students for course B? ");
-            n1 = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < n1; i++)
-            {
-                int ID = int.Parse(Console.ReadLine());
-                users.Add(new Users(ID));
-            }
-            
-            Console.Write("How many students for course A? ");
-            n1 = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < n1; i++)
-            {
-                int ID = int.Parse(Console.ReadLine());
-                users.Add(new Users(ID));
+                for (int k = 0; k < n1; k++)
+                {
+                    int ID = int.Parse(Console.ReadLine());
+                    users.Add(new Users(ID));
+                }
             }
             Console.WriteLine("Total students: " + users.Count);
         }
